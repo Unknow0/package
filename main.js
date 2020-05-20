b47a69925692ef83c12a6480f192c719d5176c04
Unknow <unknow0@free.fr>
2020-05-20 21:17:11 +0200

    fix file generation

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
 const fs=require('fs');
 
-fs.writeFileSync(require('os').homedir()+'/.m2/settings.xml', '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                      http://maven.apache.org/xsd/settings-1.0.0.xsd">
-
-  <activeProfiles>
-    <activeProfile>github</activeProfile>
-  </activeProfiles>
-
-  <profiles>
-    <profile>
-      <id>github</id>
-	  <properties>
-        <altDeploymentRepository>github::default::https://maven.pkg.github.com/Unknow0/package</altDeploymentRepository>
-      </properties>
-      <repositories>
-        <repository>
-          <id>github</id>
-          <url>https://maven.pkg.github.com/Unknow0/package</url>
-        </repository>
-      </repositories>
-    </profile>
-  </profiles>
-
-  <servers>
-    <server>
-      <id>github</id>
-      <username>Unknow0</username>
-      <password>${env.GITHUB_TOKEN}</password>
-    </server>
-  </servers>
-</settings>');
+fs.writeFileSync(require('os').homedir()+'/.m2/settings.xml', '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"'+
+  ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'+
+  ' xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">'+
+  '<activeProfiles>'+
+    '<activeProfile>github</activeProfile>'+
+  '</activeProfiles>'+
+  '<profiles>'+
+    '<profile>'+
+      '<id>github</id>'+
+	  '<properties>'+
+        '<altDeploymentRepository>github::default::https://maven.pkg.github.com/Unknow0/package</altDeploymentRepository>'+
+      '</properties>'+
+      '<repositories>'+
+        '<repository>'+
+          '<id>github</id>'+
+          '<url>https://maven.pkg.github.com/Unknow0/package</url>'+
+        '</repository>'+
+      '</repositories>'+
+    '</profile>'+
+  '</profiles>'+
+  '<servers>'+
+    '<server>'+
+      '<id>github</id>'+
+      '<username>Unknow0</username>'+
+      '<password>${env.GITHUB_TOKEN}</password>'+
+    '</server>'+
+  '</servers>'+
+'</settings>');
